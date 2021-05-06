b550d1d78e468d8eed13754f537b8fa25e56dcf5
gebrielmbelaineh <gebriel.belaineh.ug@dartmouth.edu>
2021-05-06 03:53:09 -0400

    setup done

---
+++ src/components/Nav.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import About from './About';
+import Shop from './Shop';
+
+const Nav =  () => {
+    return (
+        <nav>
+            <ul>
+                <li> About</li>
+                <li> Shop </li>
+            </ul>
+        </nav>
+    );
+}
+
+export default Nav;
--- src/index.js
+++ src/index.js
@@ -1,19 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware, compose } from 'redux';
 import App from './components/App';
-
-import reducers from './reducers';
 import './style.scss';
 
-const store = createStore(reducers, {}, compose(
-  applyMiddleware(),
-  window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : (f) => f,
-));
-
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>, document.getElementById('main'),
+    <App />, document.getElementById('main'),
 );
